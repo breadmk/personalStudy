43e4f30920b6420beaad9b445d07f03416032c96
breadmk <minvita9999@naver.com>
2021-07-14 00:12:13 +0900

    mongoDb_connect & insert

--- codingApple/node.js/server.js
+++ codingApple/node.js/server.js
@@ -5,13 +5,38 @@ const app = express();
 const bodyParser = require('body-parser');
 // app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.urlencoded({ extended: true }));
-// express.urlencoded();
 app.use(express.json());
 
-// (서버를 띄울 포트번호, 띄운후 실행할 코드)
+const MongoClient = require('mongodb').MongoClient;
+
+var db;
+MongoClient.connect('mongodb+srv://kidsnat:rmfkseldk1@cluster0.jb82a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',function(에러,client){
+
+if(에러) return console.log(에러);
+
+db = client.db('todoapp');
+
+// db.collection('post').insertOne( {_id:99, 이름: 'John', 나이:20 },function(에러,결과){
+//     console.log('저장완료');
+// });
+
+// db.collection('post').insertOne( test1 ,function(에러,결과){ 
+//     console.log(test1);
+//     console.log('저장완료');
+// }); 
+
+
+
 app.listen(8070,function(){ 
     console.log('listen on 8070');
 });
+
+})
+
+// (서버를 띄울 포트번호, 띄운후 실행할 코드)
+// app.listen(8070,function(){ 
+//     console.log('listen on 8070');
+// });
 
 //app.get('경로',fuction(요청,응답){});
 app.get('/pet',function(요청,응답){
@@ -31,8 +56,14 @@ app.get('/write',function(요청,응답){
 });
 
 app.post('/add',function(요청,응답){
-    console.log(요청.body);
+    var test = 요청.body;
+    console.log(test);
     응답.send('전송완료')
-    // 응답.send(요청.body);
+    db.collection('post').insertOne( test ,function(에러,결과){ 
+        console.log('저장완료');
+    }); 
     
 })
+
+// 어떤 사람이 /add 라는 경로로 post 요청을 하면, 데이터 2개(제목,날짜)를 보내주는데, 이 때, post라는 이름을 가진 collection에 2개 데이터를 저장하기
+// {제목:'어쩌구' , 날짜: '어쩌구}
