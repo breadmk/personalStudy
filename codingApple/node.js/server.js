b0282834cbf3f94be0fe48916442891c79068055
breadmk <minvita9999@naver.com>
2021-07-17 14:45:47 +0900

    bootstrap,update

--- codingApple/node.js/server.js
+++ codingApple/node.js/server.js
@@ -3,9 +3,13 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 // app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.set('view engine','ejs');
+app.use('/public',express.static('./public'));
+app.use(methodOverride('_method'));
 app.set('view engine','ejs');
 
 const MongoClient = require('mongodb').MongoClient;
@@ -49,11 +53,11 @@ app.get('/beauty',(요청,응답) => { //es6
 });
 
 app.get('/',function(요청,응답){
-    응답.sendFile(__dirname + '/index.html');
+    응답.render('index.ejs');
 });
 
 app.get('/write',function(요청,응답){
-    응답.sendFile(__dirname + '/write.html');
+    응답.render('write.ejs');
 });
 
 app.post('/add',function(요청,응답){
@@ -79,7 +83,7 @@ app.get('/list',function(req,res){
     db.collection('post').find().toArray(function(err,data){
         res.render('list.ejs',{ posts : data });
     });
-
+});
 app.delete('/delete',function(req,res){
     console.log(req.body);
     req.body._id = parseInt(req.body._id);
@@ -101,4 +105,9 @@ app.get('/detail/:no',function(req,res){
    
 });
     
-})
+app.get('/edit/:no',function(req,res){
+    db.collection('post').findOne( {_id : parseInt(req.params.no) }, function(err,result){
+        res.render('edit.ejs', {get:result});
+    });
+    
+});
